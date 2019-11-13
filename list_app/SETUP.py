802bc9e8cd8d44a96c8090369a25984d2359b646
Charles Obiekwe <onyiobi2@gmail.com>
2019-11-13 13:00:43 -0500

    change in directory structure

---
+++ list_app/SETUP.py
@@ -0,0 +1,7 @@
+setup(
+  name='list_app.py'
+  version='1.0',
+  packages=['sys','pickle'],
+  license='creative commons'
+  long_description=open('README.txt').read()
+)
---
+++ list_app/file2dict.py
@@ -0,0 +1,29 @@
+#! /usr/python3
+
+'''
+Convert file of lists to dictionary object and serialize dictionary object
+'''
+import sys
+import os
+import pickle
+import re
+
+
+def file2dict(file):
+  with open(file, 'r') as f:
+    f_string = f.read()
+  my_list = f_string.split('\n\n')
+  my_list = [ item.split('\n') for item in my_list ]
+  keys = [ item[0] for item in my_list ]
+  values = [item[1:] for item in my_list ]
+  my_dict = { category:items for category,items in zip(keys,values) }
+  return my_dict
+
+def main():
+  my_dict = file2dict('favorites.txt')
+  with open('favorites_dictionary.txt', 'wb') as f:
+    pickle.dump(my_dict, f)
+  print("dictionary object has been serialized")
+
+if __name__ == '__main__':
+  main()
